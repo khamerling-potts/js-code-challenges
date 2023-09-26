affaf8e59bcfe8c1f567d0e60daae037207c3a23
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-09-26 14:11:22 -0400

    4 challenges done

--- random-words-count/script.js
+++ random-words-count/script.js
@@ -9,3 +9,27 @@ const str = "Gabba boo dah ugh fpp BOO gabba doo Boo"
 console.log(convertToObjectWithCounts(str)) => { gabba: 2, boo: 3, dah: 1, ugh: 1, fpp: 1, doo: 1 }
 
 */
+
+function convertToObjectWithCounts(str) {
+  const strArr = str.split(" ");
+  const converted = {};
+  for (let word of strArr) {
+    if (word.toLowerCase() in converted) {
+      converted[word.toLowerCase()]++;
+    } else {
+      converted[word.toLowerCase()] = 1;
+    }
+  }
+  //   for (let i = 0; i < strArr.length; i++) {
+  //     if (strArr[i].toLowerCase() in converted) {
+  //       converted[strArr[i].toLowerCase()]++;
+  //     } else {
+  //       converted[strArr[i].toLowerCase()] = 1;
+  //     }
+  //   }
+  return converted;
+}
+
+console.log(
+  convertToObjectWithCounts("Gabba boo dah ugh fpp BOO gabba doo Boo")
+);
--- sorting-with-dom-manip/script.js
+++ sorting-with-dom-manip/script.js
@@ -5,10 +5,37 @@ sort the people by age in ascending order.
  */
 
 const peopleArr = [
-    { id: 1, name: 'Maria', age: 32 },
-    { id: 2, name: 'Ryan', age: 37 },
-    { id: 3, name: 'Zoe', age: 11 },
-    { id: 5, name: 'Adette', age: 9 },
-    { id: 6, name: 'Gio', age: 15 }
-  ];
-  
+  { id: 1, name: "Maria", age: 32 },
+  { id: 2, name: "Ryan", age: 37 },
+  { id: 3, name: "Zoe", age: 11 },
+  { id: 5, name: "Adette", age: 9 },
+  { id: 6, name: "Gio", age: 15 },
+];
+
+const ul = document.getElementById("list");
+const sortButton = document.getElementById("sortButton");
+
+function renderPeople() {
+  peopleArr.forEach((person) => {
+    const li = document.createElement("li");
+    li.innerText = `${person.name} is ${person.age} years old.`;
+    ul.append(li);
+  });
+}
+
+sortButton.addEventListener("click", (event) => {
+  ul.innerHTML = "";
+  peopleArr.sort((a, b) => a.age - b.age);
+  // peopleArr.sort((a, b) => {
+  //   if (a.age < b.age) {
+  //     return -1;
+  //   } else if (a.age > b.age) {
+  //     return 1;
+  //   } else {
+  //     return 0;
+  //   }
+  // });
+  renderPeople();
+});
+
+renderPeople();
